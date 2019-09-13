d1d35c9f5b27c75cdc0924a13c1cf64f8e8f505d
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2019-09-13 14:36:01 +0200

    Use session storage for caching insead of cookie

--- view/frontend/web/js/geolocation.js
+++ view/frontend/web/js/geolocation.js
@@ -1,39 +1,38 @@
-define(['jquery', 'mage/url'], function ($, urlBuilder) {
-    "use strict";
+define(['jquery', 'mage/url'], function($, urlBuilder) {
+    'use strict';
 
     return {
         /**
          * Returns two letter ISO code of users country or empty string when IP address location was not detected
          * @returns {*}
          */
-        getCountryIso: function () {
+        getCountryIso: function() {
             var graphqlUrl = urlBuilder.build('/graphql');
-            var cachedCountryIso = $.cookie('geo-location-country-iso');
-
-            if(cachedCountryIso !== undefined && cachedCountryIso !== null) {
+            var storageKey = 'mgs-geo-location';
+            var cachedCountryIso = sessionStorage.getItem(storageKey);
+            if (cachedCountryIso) {
                 return $.Deferred().resolve(cachedCountryIso);
             }
 
             return $.post({
                 url: graphqlUrl,
-                contentType: "application/json",
+                contentType: 'application/json',
                 data: JSON.stringify({
-                    query:
-                        `
+                    query: `
                             {
                                 countryGeoLocation {
                                     countryIso
                                 }
                             }
-                        `
+                        `,
                 }),
             }).then(function(result) {
                 var countryIso = result.data.countryGeoLocation.countryIso;
 
-                $.cookie('geo-location-country-iso', countryIso, { expires: 1 });
+                sessionStorage.setItem(storageKey, cachedCountryIso);
 
                 return countryIso;
             });
-        }
+        },
     };
 });
